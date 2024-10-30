ea0e4562a143198b626d4c8a74ccaf32c3e9c0cc
XionWin <xionwin@hotmail.com>
2024-10-30 15:53:19 +0800

    Update

--- platform/src/fps_counter.rs
+++ platform/src/fps_counter.rs
@@ -21,7 +21,7 @@ impl FpsCounter {
             match now_time.duration_since(self.start_time) {
                 Ok(duration) => {
                     let fps = self.counter as f32 / duration.as_secs_f32();
-                    println!("Fps: {fps:?}");
+                    println!("Fps: {fps:.1}");
                     self.reset();
                 },
                 Err(msg) => panic!("FpsCounter update error, Msg: {:?}", msg.duration())
