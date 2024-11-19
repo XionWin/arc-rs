fc9171d8791be19ca4557976dd194acf92ebdbd2
XionWin <xionwin@hotmail.com>
2024-11-19 16:43:33 +0800

    Update

--- opengl/src/texture.rs
+++ opengl/src/texture.rs
@@ -57,13 +57,6 @@ impl arc::Texture for Texture {
             Some(&image_data.value),
         );
 
-        // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
-        // First, we set the min and mag filter. These are used for when the texture is scaled down and up, respectively.
-        // Here, we use Linear for both. This means that OpenGL will try to blend pixels, meaning that textures scaled too far will look blurred.
-        // You could also use (amongst other options) Nearest, which just grabs the nearest pixel, which makes the texture look pixelated if scaled too far.
-        // NOTE: The default settings for both of these are LinearMipmap. If you leave these as default but don't generate mipmaps,
-        // your image will fail to render at all (usually resulting in pure black instead).
-
         let texture_filter = Into::<arc::TextureFilter>::into(self.texture_filter);
         crate::gl::tex_parameter_i(
             crate::def::TextureTarget::Texture2D,
@@ -76,8 +69,6 @@ impl arc::Texture for Texture {
             Into::<crate::TextureMinFilter>::into(texture_filter) as _,
         );
 
-        // Now, set the wrapping mode. S is for the X axis, and T is for the Y axis.
-        // We set this to Repeat so that textures will repeat when wrapped. Not demonstrated here since the texture coordinates exactly match
         crate::gl::tex_parameter_i(
             crate::def::TextureTarget::Texture2D,
             crate::def::TextureParameterName::TextureWrapS,
@@ -89,13 +80,6 @@ impl arc::Texture for Texture {
             crate::def::TextureWrapMode::Repeat as _,
         );
 
-        // Next, generate mipmaps.
-        // Mipmaps are smaller copies of the texture, scaled down. Each mipmap level is half the size of the previous one
-        // Generated mipmaps go all the way down to just one pixel.
-        // OpenGL will automatically switch between mipmaps when an object gets sufficiently far away.
-        // This prevents moiré effects, as well as saving on texture bandwidth.
-        // Here you can see and read about the morié effect https://en.wikipedia.org/wiki/Moir%C3%A9_pattern
-        // Here is an example of mips in action https://en.wikipedia.org/wiki/File:Mipmap_Aliasing_Comparison.png
         crate::gl::generate_mipmap(crate::def::GenerateMipmapTarget::Texture2D);
     }
 
