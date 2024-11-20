7c954bf8075d7957ca1a208fe67c3cae37101009
XionWin <xionwin@hotmail.com>
2024-11-21 02:18:47 +0800

    Update

--- arc/core/src/matrix.rs
+++ arc/core/src/matrix.rs
@@ -7,22 +7,21 @@ where T: Number {
     _value: Vec<Vec<T>>
 }
 
-fn get_default_matrix_data<T>(row_count: u32, col_count: u32) -> Vec<Vec<T>>
-where T: Number {
-    let mut result = Vec::new();
-    for _ in 0..row_count {
-        result.push(vec![T::default(); col_count as usize]);
-    }
-    result
-}
-
 impl<T> Matrix<T>
 where T: Number {
     pub fn new(row_count: u32, col_count: u32) -> Self {
         Self {
             _row_count: row_count,
             _col_count: col_count,
-            _value: get_default_matrix_data(row_count, col_count)
+            _value: Self::get_default_matrix_data(row_count, col_count)
         }
+    }
+
+    fn get_default_matrix_data(row_count: u32, col_count: u32) -> Vec<Vec<T>> {
+        let mut result = Vec::new();
+        for _ in 0..row_count {
+            result.push(vec![T::default(); col_count as usize]);
+        }
+        result
     }
 }
