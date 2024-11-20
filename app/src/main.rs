1566d9a670c11b3904b49b4fdbe206af1fbd14c5
XionWin <xionwin@hotmail.com>
2024-11-20 19:35:01 +0800

    Update

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,5 @@
 use core::Window;
-use std::{cell::Cell, rc::Rc};
+use std::cell::RefCell;
 
 fn main() {
     util::print_hight_light!("====================[ARC DEMO]====================");
@@ -7,24 +7,22 @@ fn main() {
     window.set_vsync(true);
 
     let image: Option<Box<dyn core::Image>> = None;
-    let rc_image = Rc::new(Cell::new(image));
-    let rc_image2 = rc_image.clone();
+    let image_ref = RefCell::new(image);
 
     window.run(
         |window| {
             let _g = window.get_graphic();
-            // _g.load_image_from_file("resource/image/icon96.png", core::ImageFilter::Linear)
-            rc_image.replace(Some(_g.load_image_from_file("resource/image/icon96.png", core::ImageFilter::Linear)));
+            image_ref.replace(Some(_g.load_image_from_file("resource/image/icon96.png", core::ImageFilter::Linear)));
         },
         |window| {
             let _g = window.get_graphic();
 
-            let r = rc_image2.take();
-            match &r {
+            let image = image_ref.take();
+            match &image {
                 Some(_v) => {},
                 None => util::print_panic!("image is null")
             }
-            rc_image2.set(r);
+            image_ref.replace(image);
         }
     );
 }
