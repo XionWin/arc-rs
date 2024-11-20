475abb80d58cccb684c0cc05fbc2103121648a0a
XionWin <xionwin@hotmail.com>
2024-11-21 02:40:51 +0800

    Update

--- arc/core/src/matrix.rs
+++ arc/core/src/matrix.rs
@@ -13,7 +13,7 @@ where T: Number {
         Self {
             _row_count: row_count,
             _col_count: col_count,
-            _value: Self::get_default_matrix_data(row_count, col_count)
+            _value: Self::set_as_identity(row_count, col_count, Self::get_default_matrix_data(row_count, col_count))
         }
     }
 
@@ -23,5 +23,20 @@ where T: Number {
             result.push(vec![T::default(); col_count as usize]);
         }
         result
+    }
+
+    fn set_as_identity(row_count: u32, col_count: u32, value: Vec<Vec<T>>) -> Vec<Vec<T>> {
+        let mut value = value;
+        for row in 0..row_count as usize {
+            for col in 0..col_count as usize {
+                if row == col {
+                    value[row][col] = T::one();
+                }
+                else {
+                    value[row][col] = T::default();
+                }
+            }
+        }
+        value
     }
 }
--- arc/core/src/number.rs
+++ arc/core/src/number.rs
@@ -1,13 +1,21 @@
 pub trait Number: Copy + Clone {
     fn default() -> Self;
+    fn one() -> Self;
 }
 
 impl Number for i32 {
     fn default() -> Self {
         0i32
     }
+    fn one() -> Self {
+        1i32
+    }
 }
 impl Number for f32 {
     fn default() -> Self {
         0f32
-    }}
+    }
+    fn one() -> Self {
+        1f32
+    }
+}
