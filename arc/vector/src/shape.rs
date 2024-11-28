e66a2fe460570985227d5b73a3ec482ae4381ae5
XionWin <xionwin@hotmail.com>
2024-11-29 00:14:14 +0800

    Update

--- arc/vector/src/shape.rs
+++ arc/vector/src/shape.rs
@@ -1,4 +1,14 @@
 pub trait Shape {
-    fn get_stroke_primitive(&self) -> &crate::Primitive;
-    fn get_fill_primitive(&self) -> &crate::Primitive;
+    fn get_stroke_primitive(&self) -> crate::Primitive;
+    fn get_fill_primitive(&self) -> crate::Primitive;
+}
+
+impl<T: core::Shape + ?Sized> Shape for T {
+    fn get_stroke_primitive(&self) -> crate::Primitive {
+        todo!()
+    }
+
+    fn get_fill_primitive(&self) -> crate::Primitive {
+        todo!()
+    }
 }
