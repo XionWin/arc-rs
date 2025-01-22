838ea5c68b8a638c23798c31e248ec168f08dbb2
XionWin <xionwin@hotmail.com>
2025-01-22 23:34:46 +0800

    Update

--- arc/graphic/src/graphic_shape.rs
+++ arc/graphic/src/graphic_shape.rs
@@ -10,6 +10,12 @@ impl GraphicShape {
     pub fn get_shape(&self) -> &dyn core::Shape {
         self._shape.borrow()
     }
+    pub fn set_fill_cache(&mut self, cache: Option<crate::TextureCache>) {
+        self._fill_cache = cache
+    }
+    pub fn set_stroke_cache(&mut self, cache: Option<crate::TextureCache>) {
+        self._stroke_cache = cache
+    }
 }
 
 impl From<Box<dyn core::Shape>> for GraphicShape {
