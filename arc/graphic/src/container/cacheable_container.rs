aeaaf285964aaa18cbcec09a73fcea897adff5f5
XionWin <xionwin@hotmail.com>
2025-02-05 01:03:52 +0800

    Update

--- arc/graphic/src/container/cacheable_container.rs
+++
@@ -1,6 +0,0 @@
-use core::AsAny;
-
-pub trait CacheableContainer: AsAny {
-    fn update_cache(&mut self, cache: crate::TextureCache);
-    fn get_cache(&self) -> &crate::TextureCache;
-}
--- arc/graphic/src/container/mod.rs
+++ arc/graphic/src/container/mod.rs
@@ -1,5 +1,3 @@
-mod cacheable_container;
 mod container;
 
-pub use cacheable_container::*;
 pub use container::*;
